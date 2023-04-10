e33a346817b7628221310381164f870d713a96f9
DovzhenkoVit <dovzhenko.vit@gmail.com>
2023-04-10 20:20:22 +0300

    [feat] add apps: customers, rental_agreements, vehicles, users. Set up PostgreSQL db

--- VRAS/VRAS/settings.py
+++ VRAS/VRAS/settings.py
@@ -74,9 +74,18 @@ WSGI_APPLICATION = 'VRAS.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'VRAS_db',
+        'USER': 'Vitaliy',
+        'PASSWORD': 'Vitaliy',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
